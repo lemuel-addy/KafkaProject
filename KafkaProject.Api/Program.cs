319b8922367ef1da7db4da0d498dea5411a2410f
lemuel.addy <lemuel.addy@hubtel.com>
2023-11-02 11:01:56 +0000

    Initial commit

---
+++ KafkaProject.Api/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Confluent.Kafka;
+using Newtonsoft.Json;
+
+var config = new ProducerConfig{ BootstrapServers = "localhost:9092",};
+
+using var producer = new ProducerBuilder<Null, string>(config).Build();
+try
+{ 
+    string? state;
+   while ((state = Console.ReadLine()) != null)
+    {
+        var response = await producer.ProduceAsync("kafka-topic", new Message<Null, string>
+        {
+            Value = JsonConvert.SerializeObject(new Weather(state,70)) 
+        });
+        Console.WriteLine(response.Value);
+    }
+}
+catch(ProduceException<Null, string> exception)
+{
+    Console.WriteLine(exception.Message);
+}
+
+public record Weather(string State, int Temperature);
---
+++ KafkaProject.Consumer/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Confluent.Kafka;
+using Newtonsoft.Json;
+
+var config = new ConsumerConfig
+{
+    GroupId = "kafka-consumer-group",
+    BootstrapServers = "localhost:9092",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using var consumer = new ConsumerBuilder<Null, string>(config).Build();
+
+consumer.Subscribe("kafka-topic");
+
+CancellationTokenSource token = new();
+
+try
+{
+
+    while (true)
+    {
+        var response = consumer.Consume(token.Token);
+        if (response.Message != null)
+        {
+            var weather = JsonConvert.DeserializeObject<Weather>(response.Message.Value);
+            Console.WriteLine($"State: {weather.State}" + $"Temp: {weather.Temperature}F");
+        }
+    }
+}
+catch(ConsumeException exception)
+{
+    throw;
+}
+
+
+public record Weather(string State, int Temperature);
